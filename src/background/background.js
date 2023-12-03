847f8f0af4afba67c99c1249534fc3c765f54491
AhmiiFaraz <ahmed.faraz>
2023-12-03 19:34:50 +0500

    web rtc done

--- src/background/background.js
+++ src/background/background.js
@@ -1,45 +1,45 @@
-import { getUserAuth, bgResponse } from "../assets/utils/common";
+import { getUserAuth, bgResponse } from '../assets/utils/common';
 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg == "firefox") {
-    // function handleProxyRequest(requestInfo) {
-    //   console.log(`Proxying: ${requestInfo.url}`);
-    //   return { type: "PROXY", host: "px012702.pointtoserver.com", port: 10780 };
-    // }
+// chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+//   if (msg == "firefox") {
+//     // function handleProxyRequest(requestInfo) {
+//     //   console.log(`Proxying: ${requestInfo.url}`);
+//     //   return { type: "PROXY", host: "px012702.pointtoserver.com", port: 10780 };
+//     // }
 
-    // browser.proxy.onRequest.addListener(handleProxyRequest, {
-    //   urls: ["<all_urls>"],
-    // });
-    // sendResponse(bgResponse(200, "Connected"));
-    // return true;
-    let proxySettings = {
-      proxyType: "manual",
-      ssl: "px012702.pointtoserver.com:10798",
-      socksVersion: 4,
-      httpProxyAll:true,
-    };
-    
-    browser.proxy.settings.set({ value: proxySettings });
-    sendResponse(bgResponse(200, "Connected"));
-    return true;
-  }
-});
+//     // browser.proxy.onRequest.addListener(handleProxyRequest, {
+//     //   urls: ["<all_urls>"],
+//     // });
+//     // sendResponse(bgResponse(200, "Connected"));
+//     // return true;
+//     let proxySettings = {
+//       proxyType: "manual",
+//       ssl: "px012702.pointtoserver.com:10798",
+//       socksVersion: 4,
+//       httpProxyAll:true,
+//     };
+
+//     browser.proxy.settings.set({ value: proxySettings });
+//     sendResponse(bgResponse(200, "Connected"));
+//     return true;
+//   }
+// });
 
 chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo, tab) {
-  if (tabId === tab.id && changeInfo.status === "complete") {
+  if (tabId === tab.id && changeInfo.status === 'complete') {
     if (
-      tab.url === "https://authentication.circuitvpn.com/login/proxy/success"
+      tab.url === 'https://authentication.circuitvpn.com/login/proxy/success'
     ) {
-      console.log("yaha tak ao");
+      console.log('yaha tak ao');
       chrome.cookies.get(
-        { url: "https://authentication.circuitvpn.com", name: "auth_info" },
+        { url: 'https://authentication.circuitvpn.com', name: 'auth_info' },
         (cookie) => {
           if (cookie) {
-            if (cookie.hasOwnProperty("value") !== false) {
+            if (cookie.hasOwnProperty('value') !== false) {
               const userData = JSON.parse(decodeURIComponent(cookie.value));
               chrome.storage.local.set({ userData }, () => {
                 chrome.tabs.remove(tabId);
-                console.log("User data saved to local storage");
+                console.log('User data saved to local storage');
               });
             }
           }
@@ -49,13 +49,15 @@ chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo, tab) {
     }
   }
 });
-
+chrome.privacy.network.webRTCIPHandlingPolicy.set({
+  value: 'default_public_interface_only',
+});
 chrome.webRequest.onAuthRequired.addListener(
   function (details) {
     return getUserAuth();
   },
   {
-    urls: ["<all_urls>"],
+    urls: ['<all_urls>'],
   },
-  ["blocking"]
+  ['blocking']
 );
--- src/tabs/components/Home.tsx
+++ src/tabs/components/Home.tsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect, useMemo } from "react";
-import Dashboard from "../../Layouts/Dashboard";
+import React, { useState, useEffect, useMemo } from 'react';
+import Dashboard from '../../Layouts/Dashboard';
 function Home() {
   chrome;
   const [checked, setChecked] = useState(false);
   const [loading, setLoading] = useState(false);
   const boolData = useMemo(
-    () => JSON.parse(localStorage.getItem("proxied")),
+    () => JSON.parse(localStorage.getItem('proxied')),
     [checked]
   );
 
   useEffect(() => {
     chrome.proxy.settings.get({ incognito: false }, (config) => {
-      if (config.levelOfControl === "controlled_by_this_extension") {
-        if (config.value && config.value.mode === "pac_script") {
+      if (config.levelOfControl === 'controlled_by_this_extension') {
+        if (config.value && config.value.mode === 'pac_script') {
           setChecked(true);
         }
       }
@@ -21,6 +21,7 @@ function Home() {
 
   useEffect(() => {
     if (checked) {
+      console.log( chrome.extension.inIncognitoContext)
       const pacScript = `function FindProxyForURL(url, host) {
         if (isPlainHostName(host)) {
           return 'DIRECT';
@@ -29,19 +30,26 @@ function Home() {
       }`;
 
       const config = {
-        mode: "pac_script",
+        mode: 'pac_script',
         pacScript: {
           data: pacScript,
         },
       };
       chrome.proxy.settings.set({ value: config }, () => {
-        
         if (chrome.runtime.lastError) {
-          console.error("Error setting proxy:", chrome.runtime.lastError);
+          console.error('Error setting proxy:', chrome.runtime.lastError);
         } else {
-          console.log("Proxy set successfully");
+          console.log('Proxy set successfully');
+          chrome.privacy.network.webRTCIPHandlingPolicy.set({
+            value: 'disable_non_proxied_udp',
+            scope: 'regular',
+          });
           setLoading(false);
         }
+      });
+    } else {
+      chrome.proxy.settings.clear({}, () => {
+        setLoading(false);
       });
     }
   }, [checked]);
@@ -59,46 +67,46 @@ function Home() {
 
   return (
     <Dashboard>
-      <div className="h-full grid grid-rows-3">
-        <div className="grid-cols-1">
-          <div className="flex flex-row justify-between">
-            <div className="flex flex-col">
-              <p className="text-white text-2xl">heading</p>
-              <p className="text-white text-xl">description</p>
+      <div className='h-full grid grid-rows-3'>
+        <div className='grid-cols-1'>
+          <div className='flex flex-row justify-between'>
+            <div className='flex flex-col'>
+              <p className='text-white text-2xl'>heading</p>
+              <p className='text-white text-xl'>description</p>
             </div>
             <div>image</div>
           </div>
         </div>
         <label
-          htmlFor="toggle"
-          className="flex items-center cursor-pointer relative"
+          htmlFor='toggle'
+          className='flex items-center cursor-pointer relative'
         >
-          <div className="relative">
+          <div className='relative'>
             <input
-              id="toggle"
-              type="checkbox"
-              className="sr-only"
+              id='toggle'
+              type='checkbox'
+              className='sr-only'
               checked={checked}
               onChange={handleChange}
               disabled={loading}
             />
             <div
               className={`bg-gray-600 w-14 h-8 rounded-full ${
-                loading ? "opacity-50 pointer-events-none" : ""
+                loading ? 'opacity-50 pointer-events-none' : ''
               }`}
             ></div>
             <div
               className={`absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition transform ${
-                checked ? "translate-x-full bg-green-500" : ""
-              } ${loading ? "hidden" : ""}`}
+                checked ? 'translate-x-full bg-green-500' : ''
+              } ${loading ? 'hidden' : ''}`}
             ></div>
             {loading && (
-              <div className="absolute top-0 left-0 mt-1 ml-1 animate-spin rounded-full border-t-2 border-b-2 border-gray-900 h-6 w-6"></div>
+              <div className='absolute top-0 left-0 mt-1 ml-1 animate-spin rounded-full border-t-2 border-b-2 border-gray-900 h-6 w-6'></div>
             )}
           </div>
-          <div className="ml-3 text-white font-medium">Toggle</div>
+          <div className='ml-3 text-white font-medium'>Toggle</div>
         </label>
-        <div className="grid-cols-1">3</div>
+        <div className='grid-cols-1'>3</div>
       </div>
     </Dashboard>
   );
