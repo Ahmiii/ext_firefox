a404b1999d9053ada8cb4bf409cd248a78b24b1d
AhmiiFaraz <ahmed.faraz>
2023-12-24 18:06:40 +0500

    update value

--- src/popup/root.tsx
+++ src/popup/root.tsx
@@ -1,30 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Login from '../Module/LoggingIn';
 import Dashboard from '../Module/Dashboard';
 import Location from '../Module/Location';
 import About from '../Module/Settings';
-
-// const Pages = () => {
-//   return (
-//     <Routes>
-//       <Route path='/' element={<Home />} />
-//       {/* <Route path="/about" element={<About />} /> */}
-//     </Routes>
-//   );
-// };
-
 const Root = () => {
   return (
-    <Router>
-      {/* <Pop /> */}
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/countries' element={<Location />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/settings' element={<About />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/countries' element={<Location />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/settings' element={<About />} />
+        </Routes>
+      </Router>
   );
 };
 
