ec45620bf33b1d9f73242707d4f5efde73958566
AhmiiFaraz <ahmed.faraz>
2023-12-26 19:02:13 +0500

    update value

--- src/Layouts/LandingLayout/index.tsx
+++ src/Layouts/LandingLayout/index.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 
 const LandingLayout = () => {
   return (
-    <div className="h-screen container p-4 bg-white-30 bg-[url('../assets/white_map.svg')] dark:bg-black dark:bg-[url('../assets/map.svg')] bg-no-repeat bg-center"></div>
+    <div className="h-screen container p-4 bg-white-30 bg-[url('../assets/white_map.svg')] dark:bg-black dark:bg-[url('../assets/map.svg')] bg-no-repeat bg-center">
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}
+      >
+        <img src={require(`../../assets/Logo.png`).default} />
+        <p className='text-base dark:text-white-100'>Loading...</p>
+      </div>
+    </div>
   );
 };
 
